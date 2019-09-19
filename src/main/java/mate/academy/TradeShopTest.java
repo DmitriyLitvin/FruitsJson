51af926235c016715777eec31faedbeffff30eda
chyzh <vasyachyzh1996@gmail.com>
2019-09-19 11:58:03 +0300

    added simple test

---
+++ src/main/java/mate/academy/TradeShopTest.java
@@ -0,0 +1,13 @@
+package mate.academy;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class TradeShopTest {
+
+    @Test
+    public void load() {
+        Assert.assertEquals(1,1);
+    }
+}
